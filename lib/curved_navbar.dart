d09b1849253ad513843cc8f7d2e67cb0e1abe171
Felix-101 <kupoluyitolulope452@gmail.com>
2025-02-03 20:53:30 +0100

    initial commit

---
+++ lib/curved_navbar.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+
+class CurvedBottomNavigationScreen extends StatefulWidget {
+  const CurvedBottomNavigationScreen({super.key});
+
+  @override
+  State<CurvedBottomNavigationScreen> createState() =>
+      CurvedBottomNavigationScreenState();
+}
+
+class CurvedBottomNavigationScreenState
+    extends State<CurvedBottomNavigationScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    /*  HomeScreen(), 
+    RechargeScreen(), 
+    WalletScreen(), 
+    HistoryScreen(),  */
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.white, // Background behind the navbar
+        color: Colors.red, // Navbar color
+        buttonBackgroundColor: Colors.redAccent, // Selected button color
+        height: 60,
+        animationDuration: const Duration(milliseconds: 300),
+        index: _selectedIndex,
+        items: const [
+          Icon(Icons.home, color: Colors.white, size: 30),
+          Icon(Icons.phone_android, color: Colors.white, size: 30),
+          Icon(Icons.account_balance_wallet, color: Colors.white, size: 30),
+          Icon(Icons.history, color: Colors.white, size: 30),
+        ],
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
---
+++ lib/normal_navbar.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class BottomNavScreen extends StatefulWidget {
+  const BottomNavScreen({super.key});
+
+  @override
+  State<BottomNavScreen> createState() => _BottomNavScreenState();
+}
+
+class _BottomNavScreenState extends State<BottomNavScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    /*  HomeScreen(), 
+    RechargeScreen(), 
+    WalletScreen(), 
+    HistoryScreen(),  */
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.red, // Customize as needed
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.phone_android),
+            label: 'Recharge',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance_wallet),
+            label: 'Wallet',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            label: 'History',
+          ),
+        ],
+      ),
+    );
+  }
+}
